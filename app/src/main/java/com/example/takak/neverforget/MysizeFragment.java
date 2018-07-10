7d0eab20e507fdbbd604051302b4a5a246ce765d
Takashi Kato <takakibon5@gmail.com>
2018-07-11 00:06:35 +0900

    test3

--- app/src/main/java/com/example/takak/neverforget/MysizeFragment.java
+++ app/src/main/java/com/example/takak/neverforget/MysizeFragment.java
@@ -1,18 +1,19 @@
 package com.example.takak.neverforget;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MysizeFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link MysizeFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -20,14 +21,8 @@ import android.view.ViewGroup;
 public class MysizeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public MysizeFragment() {
         // Required empty public constructor
@@ -37,72 +32,98 @@ public class MysizeFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MysizeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MysizeFragment newInstance(String param1, String param2) {
-        MysizeFragment fragment = new MysizeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static Fragment newInstance(Context context) {
+        MysizeFragment f = new MysizeFragment();
+        return f;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mysize, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        ViewGroup root = (ViewGroup)inflater.inflate(R.layout.fragment_mysize, null);
+        return root;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onResume(){
+        super.onResume();;
+        SharedPreferences prefs = this.getActivity().getSharedPreferences("mysize",
+                Context.MODE_PRIVATE);
+        int neck = prefs.getInt("neck", 0);
+        int sleeve = prefs.getInt("sleeve", 0);
+        int waist = prefs.getInt("waist", 0);
+        int insideleg = prefs.getInt("insideleg", 0);
+        EditText edText1 = (EditText) getView().findViewById(R.id.editText1);
+        if(neck != 0){
+            edText1.setText(Integer.toString(neck));
+        }
+        EditText edText2 = (EditText) getView().findViewById(R.id.editText2);
+        if(sleeve != 0){
+            edText2.setText(Integer.toString(sleeve));
+        }
+        EditText edText3 = (EditText) getView().findViewById(R.id.editText3);
+        if(waist != 0){
+            edText3.setText(Integer.toString(waist));
+        }
+        EditText edText4 = (EditText) getView().findViewById(R.id.editText4);
+        if(insideleg != 0){
+            edText4.setText(Integer.toString(insideleg));
         }
     }
-
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onPause(){
+        super.onPause();
+        EditText edText1 = (EditText) getView().findViewById(R.id.editText1);
+        EditText edText2 = (EditText) getView().findViewById(R.id.editText2);
+        EditText edText3 = (EditText) getView().findViewById(R.id.editText3);
+        EditText edText4 = (EditText) getView().findViewById(R.id.editText4);
+
+        int neck;
+        try{
+            neck = Integer.parseInt(edText1.getText().toString());
+        }
+        catch(NumberFormatException e){
+            neck = 0;
+        }
+
+        int sleeve;
+        try{
+            sleeve = Integer.parseInt(edText2.getText().toString());
+        }
+        catch(NumberFormatException e){
+            sleeve = 0;
+        }
+
+        int waist;
+        try{
+            waist = Integer.parseInt(edText3.getText().toString());
+        }
+        catch(NumberFormatException e){
+            waist = 0;
+        }
+
+        int insideleg;
+        try{
+            insideleg = Integer.parseInt(edText4.getText().toString());
+        }
+        catch(NumberFormatException e){
+            insideleg = 0;
+        }
+
+        //保存
+        SharedPreferences prefs = this.getActivity().getSharedPreferences("mysize",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("neck", neck);
+        editor.putInt("sleeve", sleeve);
+        editor.putInt("waist", waist);
+        editor.putInt("insideleg", insideleg);
+        editor.apply();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
 }
